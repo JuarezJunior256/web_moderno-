fcf663ada0a83166579a71acf63d4d7b64d40693
JuarezJunior256 <juarezjunior.live@gmail.com>
2019-04-05 01:20:21 -0300

    Método Concat

---
+++ array/concat.js
@@ -0,0 +1,6 @@
+//concat, concatena varios arrays em um novo array
+const filhas = ['Valesk', 'Eduarda']
+const filhos = ['Pedro', 'Junior']
+
+const todos = filhas.concat(filhos)
+console.log(todos);
